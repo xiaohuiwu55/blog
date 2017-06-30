dc73ef6c624e5bd6729954d8d2f342e53550665a
xiaowu <xiaohuiwu521@gmail.com>
2017-06-30 20:49:49 +0800

    edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,8 +18,28 @@ class PostsController < ApplicationController
   else
     render 'new'
   end
-end 
+end
 
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(params[:post].permit(:title,:body))
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to root_path
+end
   private
 
   def post_params
